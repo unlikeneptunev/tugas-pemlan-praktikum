58316dfc6f8bac93d8a7d6fd7198becf347c6532
Ahmad Hafidz <211817240+unlikeneptunev@users.noreply.github.com>
2025-05-19 09:47:32 +0700

    Update FileSize.java
    Add comments to line 14 and line 16

--- pemlanTugas9/Nomor 2/FileSize.java
+++ pemlanTugas9/Nomor 2/FileSize.java
@@ -1,17 +1,19 @@
 package nomor2;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.io.IOException;
 
 public class FileSize {
+
     public static void main(String[] args) {
-        // Menentukan path file (ganti dengan path yang sebenarnya)
-        Path path = Paths.get("path\\to\\file\\testSize.txt"); 
+        Path path = Paths.get("C:\\Users\\src\\Documents\\Kuliah\\SEMESTER 2\\Pemrograman Lanjut\\Tugas\\Praktikum_Bab10\\Operasi File\\src\\nomor2\\testSize.txt");
         try {
             long fileSizeInBytes = Files.size(path);
+            // 1 KB = 1024 Bytes
             long fileSizeInKB = fileSizeInBytes / 1024;
+            // 1 MB = 1024 KB
             long fileSizeInMB = fileSizeInKB / 1024;
             System.out.println("Ukuran file dalam KB: " + fileSizeInKB + " KB");
             System.out.println("Ukuran file dalam MB: " + fileSizeInMB + " MB");
